90e5d13cd62b5d8aeae4fe8768631cf9c8e3dd3d
aseaboyer <aseaboyer@gmail.com>
2016-06-12 23:20:37 -0700

    Cloned bit-mite, started mathing orbits

---
+++ js/Game.js
@@ -0,0 +1,98 @@
+function Game (canvas) {
+    var obj = {},
+        now = Date.now ();
+    
+    obj.paused = false;
+    obj.canvas = canvas;
+    obj.context = canvas.getContext ("2d");
+    obj.bitSize = 4;
+    obj.board = {
+      height:  (canvas.height / obj.bitSize), 
+      width:  (canvas.width / obj.bitSize)
+    };
+    obj.purse = document.getElementById ("coinCount");
+    
+    obj.frame = {
+        current: 0,
+        frequency: 50,
+        last: now,
+        update: function () {
+            this.current++;
+            this.last = Date.now ();
+        }
+    };
+    // Replace frame with time to use deltatime
+    obj.time = {
+        current: now,
+        delta: 0,
+        frequency: 50,
+        last: now,
+        update: function () {
+            var n = Date.now ();
+            this.last = this.current;
+            this.current = n;
+            this.delta = this.current - this.last;
+        }
+    };
+    obj.colors = {
+        player: "47,181,243",
+        enemy: "252,130,195",
+        coin: "225,200,41",
+        spawningEnemy: "0,0,0"
+    };
+    
+    obj.clearCanvas = function () {
+        this.canvas.height = this.canvas.clientHeight;
+        this.canvas.width = this.canvas.clientWidth;
+        this.context.clearRect (0, 0, this.canvas.width, this.canvas.height);
+    };
+    
+    obj.drawBit = function (x, y, color, alpha) {
+        var color = "rgba(" + color + "," + alpha + ")";
+        obj.context.fillStyle = color;
+        obj.context.fillRect ( (x * obj.bitSize), (y * obj.bitSize),
+            obj.bitSize, obj.bitSize);
+    };
+    
+    obj.purse = document.getElementById ("purse");
+    obj.coin = {};
+    obj.coinsCollected = 0;
+    obj.spawnCoin = function () {
+        var x = Math.floor(Math.random() * this.board.width),
+            y = Math.floor(Math.random() * this.board.height);
+        this.coin = new Coin (x, y);
+    };
+    obj.collectCoin = function () {
+        this.coinsCollected++;
+        this.spawnCoin ();
+        this.purse.innerHTML = this.coinsCollected;
+        player.changeHealth (3);
+    };
+    
+    obj.enemies = new Array ();
+    obj.lastSpawnedEnemy = 0;
+    obj.maxSpawnFrequency = 2000;
+    obj.spawnEnemy = function () {
+        var x = Math.floor(Math.random() * this.board.width),
+            y = Math.floor(Math.random() * this.board.height);
+        this.enemies.push (new Enemy (x, y));
+        this.lastSpawnedEnemy = Date.now ();
+    };
+    obj.removeEnemy = function (num) {
+        this.enemies.splice (num, 1);
+        //this.spawnEnemy ();
+    };
+    
+    document.body.dataset.killScreen = "false";
+    obj.killScreen = false;
+    obj.openKillScreen = function () {
+        document.body.dataset.killScreen = "true";
+        this.paused = true;
+    };
+    
+    obj.restart = function () {
+        location.reload ();
+    };
+    
+    return obj;
+}
---
+++ js/run.js
@@ -0,0 +1,192 @@
+/*
+ * Vars
+ */
+var game, keys,
+    player;
+var planet = {
+    "orbit": 1.2,
+    "radius": 20,
+    "position": {
+        "x": 200,
+        "y": 200
+    }
+};
+var moon = {
+    "orbit": 1.4,
+    "radius": 6,
+    "planet": planet,
+    "speed":0.1,
+    "angle":0
+};
+/*
+ * Key presses
+ */
+/*document.body.addEventListener("keydown", function (e) {
+    keys.press (e.keyCode);
+});
+document.body.addEventListener("keyup", function (e) {
+    keys.release (e.keyCode);
+});
+document.getElementById ("restart").addEventListener("click", function (e) {
+    game.restart ();
+});*/
+
+/*
+ * INIT Phase
+ */
+(function () { // INIT
+    var c = document.getElementById ("game");
+    game = new Game (c);
+    keys = new Keyring ();
+    
+    
+    player = new Player ();
+    
+    /*
+    game.spawnCoin ();
+    game.spawnEnemy ();
+    */
+    Draw ();
+    GameLoop ();
+})();
+
+/*
+ * UPDATE Loop
+ */
+function GameLoop () {
+    window.requestAnimationFrame(GameLoop);
+    
+    var now = Date.now ();
+    
+    // get sthe size of the body/html/screen, set the canvas dimmensions to taht size
+    
+    if (now >= (game.time.last + game.time.frequency) &&
+       game.paused === false) {
+        /*
+            Updates
+        */
+        game.time.update ();
+
+        moon.angle += moon.speed * game.time.delta * 0.01;
+        
+        //player.move (keys.current.state);
+
+        /*
+            Execute Draws
+        */
+        Draw ();
+    }
+    
+    //console.log (keys.current);
+};
+
+function Draw () {
+    // clear and reset canvas
+    game.clearCanvas ();
+    
+    
+    /* TEST DRAW A GREEN PLANET */
+    var radius = 10;
+    
+    /* Added to clearCanvss
+    canvas.height = canvas.clientHeight;
+    canvas.width = canvas.clientWidth;
+    */
+    game.context.beginPath();
+    game.context.arc(planet.position.x, planet.position.y, planet.radius, 0, 2 * Math.PI, false);
+    game.context.fillStyle = 'green';
+    game.context.fill();
+    game.context.lineWidth = 0;
+    game.context.strokeStyle = '#003300';
+    game.context.stroke();
+    
+    /* DRAW A TEST SATELITE */
+    var satLoc = orbitPosition (moon.angle, moon.orbit * planet.radius, planet.position.x, planet.position.y);
+    game.context.beginPath();
+    game.context.arc(satLoc.x, satLoc.y, moon.radius, 0, 2 * Math.PI, false);
+    game.context.fillStyle = 'tomato';
+    game.context.fill();
+    game.context.lineWidth = 0.2;
+    game.context.strokeStyle = '#666666';
+    game.context.stroke();
+    
+    
+    /*
+    // player
+    for (var j = 0; j < (player.history.length >= 4 ? 4 : player.history.length); j++) {
+        game.drawBit (player.history [j].x, player.history [j].y,
+                  game.colors.player, "0." + (-j+5));
+    }
+    game.drawBit (player.position.x, player.position.y,
+                  game.colors.player, "1");
+    
+    // coin actions
+    if (player.position.x === game.coin.x &&
+        player.position.y === game.coin.y) {
+        game.collectCoin ();
+        
+    } else {
+        game.drawBit (game.coin.x, game.coin.y,
+                  game.colors.coin, "1");
+    }
+    
+    // enemies
+    for (var i = 0; i < game.enemies.length; i++) {
+        game.enemies [i].update ();
+        
+        // Check to see if it's offscreen - use player check
+        if (game.enemies [i].position.x >= 0 &&
+            game.enemies [i].position.x < game.board.width &&
+            game.enemies [i].position.y >= 0 &&
+            game.enemies [i].position.y < game.board.height) {
+            
+            game.drawBit (game.enemies [i].position.x, 
+                game.enemies [i].position.y,
+                game.colors.enemy, "1");
+            for (var j = 0; j < (game.enemies [i].history.length >= 4 ? 4 : game.enemies [i].history.length); j++) {
+                if (game.enemies [i].spawning) {
+                    game.drawBit (game.enemies [i].history [j].x,
+                            game.enemies [i].history [j].y,
+                            game.colors.spawningEnemy, "0." + (-j+5));
+                } else {
+                    game.drawBit (game.enemies [i].history [j].x,
+                            game.enemies [i].history [j].y,
+                            game.colors.enemy, "0." + (-j+5));
+                }
+            }
+        } else {
+            game.removeEnemy (i);
+        }
+    }
+    
+    // check for enemy impacts with player
+    for (var i = 0; i < game.enemies.length; i++) {
+        if (game.enemies [i].position.x === player.position.x &&
+            game.enemies [i].position.y === player.position.y &&
+            game.enemies [i].spawning === false) {
+                player.changeHealth (-30);
+        }
+    }
+    
+    
+    // spawn a new enemy - progressively more enemies
+    if (game.enemies.length < (game.coinsCollected/2) ||
+        game.enemies.length <= 0) {
+        game.spawnEnemy ();
+    }*/
+}
+
+
+
+function orbitPosition (angle, radius, cx, cy) {
+    c = Math.cos(angle);
+    s = Math.sin(angle);
+    /*document.getElementById("cir").style.left=x
+    document.getElementById("cir").style.top=y
+    document.getElementById("circle").innerHTML="Circle x = "+parseInt(x)
+    document.getElementById("circle2").innerHTML="Circle y = "+parseInt(y)*/
+    return {
+        "x": (radius * c) + cx,
+        "y": (radius * s) + cy
+    };
+}
